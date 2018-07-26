693f3e7d45c1367114db6d44e472b093b6bc9f37
Gordonbalaaka <balaakagordon@gmail.com>
2018-07-26 21:51:08 +0300

    Update app.py

--- mydiaryapp/app.py
+++ mydiaryapp/app.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request, \
     flash, redirect
 from flask_restful import Resource, Api
 import datetime
+import mydiary
 from mydiary import DiaryEntry
 
 
