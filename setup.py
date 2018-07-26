932a25e7caf390ea142a7124b40a1da8667ba1ea
Gordonbalaaka <balaakagordon@gmail.com>
2018-07-26 09:37:12 +0300

    Update Travis file.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='travistest',
-    packages=['travistest'],
-    version='0.0.8',
-    description='Test project to get acquainted with TravisCI',
-    url='https://github.com/lqdev/TravisTest',    
-)
