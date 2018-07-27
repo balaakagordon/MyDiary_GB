2723cf5e0847728af3d61ee9254437e2ba38992c
Gordonbalaaka <balaakagordon@gmail.com>
2018-07-27 06:04:26 +0300

    Update travis file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+
+setup(
+    name='MyDiary_GB',
+    packages=find_packages,
+    version='0.0.1',
+    description='Test project with TravisCI',
+    url='https://github.com/lqdev/TravisTest',    
+)
